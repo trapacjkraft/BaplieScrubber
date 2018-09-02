14ca2765025cf977a6f9c06e2db9290ce614d95f
Joshua Kraft <joshua@auswks2.trapac.com>
2018-09-02 01:56:53 -0500

    Initial Commit

---
+++ BaplieScrubber/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BaplieScrubber
+//
+//  Created by Joshua Kraft on 9/2/18.
+//  Copyright © 2018 Joshua Kraft. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BaplieScrubber/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BaplieScrubber
+//
+//  Created by Joshua Kraft on 9/2/18.
+//  Copyright © 2018 Joshua Kraft. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
